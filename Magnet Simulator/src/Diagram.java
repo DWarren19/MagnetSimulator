82330f19b71788c659169c023559858593342b40
19warren_D <19warren_D@bexleygs.co.uk>
2025-06-05 10:15:28 +0100

    edited Diagram

--- Magnet Simulator/src/Diagram.java
+++ Magnet Simulator/src/Diagram.java
@@ -5,11 +5,19 @@ public class Diagram extends JPanel {
     public double[] data;
     public int magnetWidth;
     public double[] outputData;
+    public JLabel[] outputLabels;
     public Diagram(double[] d){
         super();
         setBorder(BorderFactory.createLineBorder(Color.black));
         data = d;
         this.outputData = d;
+        setBounds(10, 250, 330, 220);
+        outputLabels = new JLabel[4];
+        String[] labelData = {"IR", "OR", "L", "S"};
+        for (int i = 0; i < 4; i++) {
+            outputLabels[i] = new JLabel(labelData[i]);
+            add(outputLabels[i]);
+        }
     }
     public void setMagnetData(double[] d) {
         Graphics g = super.getGraphics();
@@ -49,9 +57,14 @@ public class Diagram extends JPanel {
         g2.fillRect(160+(int)(outputData[1]), 100-(int) outputData[3], (int)(magnetWidth/2- outputData[1]), (int) outputData[3]*2);
         g2.setColor(Color.lightGray);
         g2.fillRect(160+(int)(outputData[1]), 100-(int) outputData[2], (int)(magnetWidth/2- outputData[1]), (int) outputData[2]*2);
+
         g2.fillRect(160+(magnetWidth/2)+6, 120-(int) outputData[2], 8, (int) outputData[2]-20);
         g2.fillPolygon(new int[]{160+(magnetWidth/2)+1, 160+(magnetWidth/2)+19, 160+(magnetWidth/2)+10}, new int[]{120-(int) outputData[2], 120-(int) outputData[2], 100-(int) outputData[2]}, 3);
         g2.fillRect(160-magnetWidth/2-14, 120-(int) outputData[3], 8, (int) outputData[3]-20);
         g2.fillPolygon(new int[]{160-magnetWidth/2-19, 160-magnetWidth/2-1, 160-magnetWidth/2-10}, new int[]{120-(int) outputData[3], 120-(int) outputData[3], 100-(int) outputData[3]}, 3);
+        outputLabels[0].setBounds(160+(magnetWidth/2)+5, 100-(int) outputData[2], 20, 20);
+        outputLabels[1].setBounds(160-(magnetWidth/2)-19, 100-(int) outputData[3], 20, 20);
+
+        g2.fillRect(160-magnetWidth/2, 200, magnetWidth, 10);
     }
 }
